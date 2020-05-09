2821e8bdb3dbcaf2168622e111759635f6d6c531
arielff3 <arielfrancoferreira4@gmail.com>
2020-05-08 22:37:16 -0300

    Desafio Completo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
+import api from './services/api'
 import "./styles.css";
 
 function App() {
+
+  const [respositories, setRepoList] = useState([])
+
+  useEffect(() => {
+    const listRepositories = async () => {
+      const response = await api.get('/repositories')
+      console.log(response);
+      setRepoList(response.data);
+    }
+
+    listRepositories();
+  }, [respositories])
+
   async function handleAddRepository() {
-    // TODO
+    await api.post('/repositories', {
+      title: "Desafio React",
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
   }
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {respositories.map(repo => 
+          <li key={repo.id}>
+          {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        )}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
